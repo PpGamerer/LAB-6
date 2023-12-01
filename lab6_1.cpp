4f35535715a835ad9554ca409666d82e23f05086
Pp <Pimolnat_ka@cmu.ac.th>
2023-12-01 15:11:10 +0700

    "lab6_1.cpp completed"

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,25 @@
 using namespace std;
 
 int main(){
+int num;
+cout << "Enter an integer: ";
+cin >> num;
+
+int even = 0;
+int odd = 0;
+
+while(num != 0){
+    if(num%2==0){
+        even += 1;
+     } else {
+        odd += 1;
+     }
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> num;
+} 
+   
+    cout << "#Even numbers = " << even << endl;
+    cout << "#Odd numbers = " << odd;
     return 0;
 }
+
